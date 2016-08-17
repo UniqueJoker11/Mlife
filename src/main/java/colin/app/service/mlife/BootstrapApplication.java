87164040b545c131e005de3f92ff0b920de89402
UniqueJoker11 <yongyuandexiaochou@126.com>
2016-08-17 23:12:32 +0800

    Initial commit

---
+++ src/main/java/colin/app/service/mlife/BootstrapApplication.java
@@ -0,0 +1,14 @@
+package colin.app.service.mlife;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by joker on 16/8/17.
+ */
+@SpringBootApplication
+public class BootstrapApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
